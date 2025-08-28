0cf600c29d9a4a3fabc2b1a8fd87fa88683d37ae
Momina45 <duakunwar45@gmail.com>
2025-08-27 19:01:39 -0700

    save the cart in localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,18 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+    if(!cart){
+        cart = 
+    [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity:2
 },{
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity:1
 }];
-
+    }
+    function saveToStorage(){
+        localStorage.setItem('cart',JSON.stringify(cart));
+    }
 export function addToCart(productId){
         let selecterQuantity = document.querySelector(`.js-quantity-${productId}`)|| 1;
         let quantity = Number(selecterQuantity.value) || 1;
@@ -23,7 +30,9 @@ export function addToCart(productId){
             cart.push({
                 productId,
                 quantity
-        });}
+        });
+        }
+        saveToStorage();
     }
 
     export function RemovefromCart(productId){
@@ -34,4 +43,6 @@ export function addToCart(productId){
             }
         })
         cart = newCart;
+
+        saveToStorage();
     }
